600d4dca8a60db0604dad0898443e2ae578a4099
Adir <adiramsalem@gmail.com>
2015-02-28 17:38:49 +0200

    refactor(configuration): remove lodash from angular-ui-router configuration

--- configurations/angular-ui-router.js
+++ configurations/angular-ui-router.js
@@ -1,27 +1,31 @@
-var _ = require('lodash');
-
 module.exports = {
     parser: {
         isRoute: function(node) {
             function recursiveSearch(node) {
                 return (node && node.callee) &&
                     (
-                        (node.callee.type === 'MemberExpression' &&
-                        node.callee.object.name === '$stateProvider' &&
-                        node.callee.property.name === 'state' &&
-                        node.arguments.length === 2 &&
-                        node.arguments[1].type === 'ObjectExpression')
+                    (node.callee.type === 'MemberExpression' &&
+                    node.callee.object.name === '$stateProvider' &&
+                    node.callee.property.name === 'state' &&
+                    node.arguments.length === 2 &&
+                    node.arguments[1].type === 'ObjectExpression')
                     ||
-                        recursiveSearch(node.callee.object)
+                    recursiveSearch(node.callee.object)
                     )
             }
 
             return recursiveSearch(node);
         },
         extractRoute: function(node) {
-            return _.find(node.arguments[1].properties, function(property) {
-                return property.key.name === 'url';
-            }).value.value;
+            return node.arguments[1].properties
+                .filter(function(property) {
+                    return property.key.name === 'url';
+                })
+                .map(function(property) {
+                    return property.value.value
+                })
+                .pop();
+
         }
     }
 };
